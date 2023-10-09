32ad1d7aeff80a55d771da89c2a63edb7c3a9bde
Noah <noahsu@chalmers.se>
2023-10-09 14:58:56 +0200

    Början på nåt gött

--- Labb_3/server.erl
+++ Labb_3/server.erl
@@ -4,16 +4,16 @@
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    gen_server:start(ServerAtom).
+    genserver:start(ServerAtom,[], fun handler/2).
     % TODO Implement function
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
+handler(ServerAtom, {St, Channel}) ->
     not_implemented.
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+    genserver:request(ServerAtom, kill_channels),
+    genserver:stop(ServerAtom).
