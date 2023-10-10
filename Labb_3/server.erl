1cd0f9236b3c375682d20d4d5fbdb6dc59aeaf08
Noah <noahsu@chalmers.se>
2023-10-10 15:33:50 +0200

    Server.erl börjat hantera join

--- Labb_3/server.erl
+++ Labb_3/server.erl
@@ -4,13 +4,19 @@
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    genserver:start(ServerAtom,[], fun handler/2).
+    genserver:start(ServerAtom,[], fun handle_ser/2).
     % TODO Implement function
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
-handler(ServerAtom, {St, Channel}) ->
-    not_implemented.
+handle_ser(State, {join, Ch, Client}) ->
+    case lists:member(Ch, State) of
+        true -> res = genserver:request(list_to_atom(Ch),{join,Client}),
+        cas res of 
+            joined -> 
+        end,
+        false -> 
+    end
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
